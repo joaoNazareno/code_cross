99f4a37406a4293eaec81ed23cb9580b16788684
Joao <joaonaza24@icloud.com>
2024-08-17 20:52:29 -0300

    Tela_inicial

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="/src/assets/code_cross.png" width="700px" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p />
+  <h5><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+      style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
+      <path
+        d="M12 22c5.421 0 10-4.579 10-10S17.421 2 12 2 2 6.579 2 12s4.579 10 10 10zm0-18c4.337 0 8 3.663 8 8s-3.663 8-8 8-8-3.663-8-8 3.663-8 8-8z">
+      </path>
+      <path
+        d="M12 17c.901 0 2.581-.168 3.707-1.292l-1.414-1.416C13.85 14.735 12.992 15 12 15c-1.626 0-3-1.374-3-3s1.374-3 3-3c.993 0 1.851.265 2.293.707l1.414-1.414C14.582 7.168 12.901 7 12 7c-2.757 0-5 2.243-5 5s2.243 5 5 5z">
+      </path>
+    </svg>. 2024 Code Cross, inc.</h5>
 </template>
 
 <style scoped>
+h5 {
+  display: flex;
+  place-items: center;
+  align-items: center;
+  margin-left: 530px;
+}
+
 header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.logo {}
 
 @media (min-width: 1024px) {
   header {
@@ -35,7 +44,7 @@ header {
   }
 
   .logo {
-    margin: 0 2rem 0 0;
+    margin: 0 5rem 0 0;
   }
 
   header .wrapper {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,15 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">
+      <img alt="Vue logo" class="logo jump" src="/src/assets/play_code_cross.png" width="250px" />
+    </a>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
 .greetings h1,
 .greetings h3 {
   text-align: center;
@@ -39,6 +19,28 @@ h3 {
   .greetings h1,
   .greetings h3 {
     text-align: left;
+  }
+}
+
+.logo {
+  display: block;
+  margin: 0 auto;
+}
+
+.jump {
+  animation: jump 1s infinite;
+}
+
+.jump:hover {
+  animation-play-state: paused;
+}
+
+@keyframes jump {
+  0%, 100% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
   }
 }
 </style>
